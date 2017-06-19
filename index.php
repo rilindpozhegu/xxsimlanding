167e2eec972bd1383d75329c0859ca4107fb525a
rilindpozhegu <rilindp@gmail.com>
2017-06-19 15:13:09 +0200

    new updates

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="No roaming charges sim card, No roaming charges, Roaming sim card, No international, roaming charges, Free roaming worldwide, Solution to roaming charges, TravelSim, WorldSim">
 
-    <title>XXSIM LandingPage</title>
+    <title>International Sim Card</title>
 
     <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" type="text/css" href="build/styles.css?v=1.1">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="build/styles.css?v=1.1">
 </head>
 
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
@@ -28,13 +28,13 @@
     <div class="container">
         <div class="row">            
             <div class="col-md-12 navigationbar_section_container">                
-                <img src="img/logo/logo1-01.svg">
+                <img src="img/logo/logo2.png">
             </div>
             <div class="main_cover_text">
                 <div class="col-md-7">
-                    <h2>Forget about your phone bill and <span>roaming charges</span></h2>
-                    <p>Why waste your time on finding free Wi-Fi or on buying local sim cards when you can start calling and browsing from the moment you set foot at your destination.</p>
-                    <button class="yellow_btn">Order Now</button>
+                    <h2 class="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".2s">Forget about your phone bill and <span>roaming charges</span></h2>
+                    <p class="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".3s">Why waste your time on finding free Wi-Fi or on buying local SIM cards when you can start calling and browsing from the moment you set foot at your destination.</p>
+                    <a href="https://xxsim.com/en/Online_Shop/" target="_blank"><button class="yellow_btn wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".4s">Order Now</button></a>
                 </div>
             </div>
         </div>
@@ -47,11 +47,11 @@
     <div class="container">
         <div class="row">
             <div class="col-md-7">
-                <h3>Request A Call Back To Know More About This Amazing Sim Card</h3>
+                <h3>Request A Call Back To Know More About This Amazing SIM Card</h3>
             </div>
             <div class="col-md-5 col-sm-12">
                 <input type="name" name="" placeholder="Name">
-                <input type="phone" name="" placeholder="Phone">
+                <input type="phone" name="" placeholder="Phone(00xxx)">
                 <button>Submit</button>
             </div>
         </div>
@@ -64,33 +64,34 @@
 <section class="sevices_section">
     <div class="container">
         <div class="row">
-            <h2>Benefits of using <span>XXSIM</span></h2>
-            <h5>Why waste your time on finding free Wi-Fi or on buying local sim cards when you can<br> start calling and browsing from the moment you set foot at your destination.</h5>
-            <div class="icons_section_pan_c row">
+            <h2 class="wow fadeInDown" data-wow-duration="0.5s" data-wow-delay=".1s">Benefits of using <span>XXSIM</span></h2>
+            <h5 class="wow fadeInDown" data-wow-duration="0.5s" data-wow-delay=".2s">Why waste your time on finding free Wi-Fi or on buying local SIM cards when you can<br> start calling and browsing from the moment you set foot at your destination.</h5>
+            <div class="icons_section_pan_c row" style="margin-left: 0px; margin-right: 0px;">
                 <div class="offset-md-1 col-md-2 col-sm-4 col-6">
-                    <img src="img/icons/1-01.svg">
+                    <img src="img/icons/1-01.png" class="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".2s">
                     <p>No incoming call charges in over 150 countries.</p>
                 </div>
                 <div class="col-md-2 col-sm-4 col-6">
-                    <img src="img/icons/2-01.svg">
+                    <img src="img/icons/2-01.png" class="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".3s">
                     <p>Outgoing calls as low as €0.19 worldwide</p>
                 </div>
                 <div class="col-md-2 col-sm-4 col-6">
-                    <img src="img/icons/3-01.svg">
-                    <p>Free call forwarding to your national sim card.</p>
+                    <img src="img/icons/3-01.png" class="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".4s">
+                    <p>Free call forwarding to your national SIM card.</p>
                 </div>
                 <div class="col-md-2 col-sm-4 col-6">
-                    <img src="img/icons/4-01.svg" style="width: 80%; margin-top: -17px;">
+                    <img src="img/icons/star4.png" class="bounce1 smallstar">
+                    <img src="img/icons/4-01.png" class="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".5s" id="jumping_icon">
                     <p>With the optional Swiss landline number(+4155...), it's free to call from almost everywhere.</p>
                 </div>
-                <div class="col-md-2 col-sm-4 col-6">
-                    <img src="img/icons/5-01.svg">
+                <div class="col-md-2 col-sm-4 col-6 offset-3 offset-md-0">
+                    <img src="img/icons/5-01.png" class="wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".6s">
                     <p>No contracts, no monthly charges</p>
                 </div>
             </div> 
-            <div class="services_buttons_all">
-                <button class="yellow_btn" style="margin: 3px;">View All</button>
-                <button class="dark_btn" style="margin: 3px;">Rates and Coverage</button>
+            <div class="services_buttons_all wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".2s">
+                <a href="https://xxsim.com/en/why/" target="_blank"><button class="yellow_btn" style="margin: 3px;">View All</button></a>
+                <a href="https://xxsim.com/en/Rates_and_Coverage/" target="_blank"><button class="dark_btn" style="margin: 3px;">Rates and Coverage</button></a>
             </div>   
         </div>
     </div>
@@ -99,12 +100,16 @@
 <section class="phone_section_hom">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6 color_phon">
+            <div class="col-md-6 no_padding hidden-md-up">
+                <img src="img/22.png" class="img-fluid">
+            </div>
+            <div class="col-md-6 color_phon wow zoomIn" data-wow-duration="0.5s" data-wow-delay=".4s">
+                <h1>LARGE ACCOUNT</h1>
                 <h2>Cut roaming charges for employees on business trips with our corporate account</h2>
-                <a href="https://xxsim.com/"><button class="yellow_btn">Visit Website</button></a>
+                <a href="https://xxsim.com/en/Business_Customer/Home/" target="_blank"><button class="yellow_btn">Visit Website</button></a>
             </div>
             <div class="col-md-6 no_padding">
-                <img src="img/22.png" class="img-fluid">
+                <img src="img/22.png" class="img-fluid hidden-md-down">
             </div>
         </div>
     </div>
@@ -116,12 +121,12 @@
 <section class="shopping_section">
     <div class="container">
         <div class="row">
-            <div class="col-md-5 offset-md-1">
-                <img src="img/icons/66-01.svg">
+            <div class="col-md-5 offset-md-1 wow fadeInLeft" data-wow-duration="0.5s" data-wow-delay=".2s">
+                <img src="img/icons/66-01.png">
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 wow fadeInRight" data-wow-duration="0.5s" data-wow-delay=".4s">
                 <h2>Shipping is <br> completely free</h2>
-                <a href="https://xxsim.com/en/Online_Shop/"><button class="dark_btn">Order Now</button></a>
+                <a href="https://xxsim.com/en/Online_Shop/" target="_blank"><button class="dark_btn">Order Now</button></a>
             </div>
         </div>
     </div>
@@ -131,38 +136,40 @@
 <footer class="footer_section">
     <div class="container">
         <div class="row">
-            <div class="col-md-2">
+            <div class="col-md-2 mobile-d-n">
                 <h5>PRIVATE CUSTOMERS</h5>
-                <a href=""><p>Home</p></a>
-                <a href=""><p>Rates and Coverage</p></a>
-                <a href=""><p>Online Shop</p></a>
-                <a href=""><p>Recharge</p></a>
-                <a href=""><p>Support</p></a>
+                <a href="https://xxsim.com/en/page/view.html?id=126"><p>Home</p></a>
+                <a href="https://xxsim.com/en/Private_Customer/Rates_and_Coverage/"><p>Rates and Coverage</p></a>
+                <a href="https://xxsim.com/en/Private_Customers/Online_Shop/"><p>Online Shop</p></a>
+                <a href="https://xxsim.com/en/Private_Customers/Recharge/&step=1&purchase[type]=airtime"><p>Recharge</p></a>
+                <a href="https://xxsim.com/en/Support/"><p>Support</p></a>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-2 mobile-d-n">
                 <h5>BUSINESS CUSTOMERS</h5>
-                <a href=""><p>Home</p></a>
-                <a href=""><p>What is XXSIM?</p></a>
-                <a href=""><p>Benefits</p></a>
-                <a href=""><p>Order</p></a>
-                <a href=""><p>Support</p></a>
+                <a href="https://xxsim.com/en/page/view.html?id=163"><p>Home</p></a>
+                <a href="https://xxsim.com/en/Business_Customer/What_is_XXSIM_/"><p>What is XXSIM?</p></a>
+                <a href="https://xxsim.com/en/Business_Customer/Benefits/"><p>Benefits</p></a>
+                <a href="https://xxsim.com/en/Business_Customer/Order/"><p>Order</p></a>
+                <a href="https://xxsim.com/en/Support/"><p>Support</p></a>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-2 mobile-d-n">
                 <h5>PARTNERS</h5>
-                <a href=""><p>Become a reseller</p></a>
-                <a href=""><p>Affiliation</p></a>
-                <a href=""><p>Press</p></a>
+                <a href="https://xxsim.com/en/PARTNERS/Become_a_reseller/"><p>Become a reseller</p></a>
+                <a href="https://xxsim.com/en/PARTNERS/Affiliation/"><p>Affiliation</p></a>
+                <a href="https://xxsim.com/en/PARTNERS/Press/"><p>Press</p></a>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-2 mobile-d-n">
                 <h5>LEGAL INFORMATION</h5>
-                <a href=""><p>XXSIM, About us</p></a>
-                <a href=""><p>General Sales Conditions</p></a>
-                <a href=""><p>Privacy Policy</p></a>
-                <a href=""><p>Terms and Conditions</p></a>
-                <a href=""><p>Press</p></a>
+                <a href="https://xxsim.com/en/LEGAL_INFORMATION/XXSIM__About_us/"><p>XXSIM, About us</p></a>
+                <a href="https://xxsim.com/en/LEGAL_INFORMATION/General_Sales_Conditions/"><p>General Sales Conditions</p></a>
+                <a href="https://xxsim.com/en/LEGAL_INFORMATION/Privacy_Policy/"><p>Privacy Policy</p></a>
+                <a href="https://xxsim.com/en/LEGAL_INFORMATION/Terms_and_Conditions/"><p>Terms and Conditions</p></a>
             </div>
             <div class="col-md-2 col-12 offset-md-2">
-                <img src="img/logo/logo2-01.svg">
+                <img src="img/logo/logo2-01.png">
+            </div>
+            <div class="col-md-4 col-12" style="margin-top: 30px;">
+                <a href="http://www.emiratesgraphic.com/"><p>© Copyright By Emirates Graphic 2017</p></a>
             </div>
         </div>
     </div>
